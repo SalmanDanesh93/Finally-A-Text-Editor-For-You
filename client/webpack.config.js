97213e49088480d185a79f75623838f588bc850e
SalmanDanesh93 <salmand64@Yahoo.com>
2022-12-05 21:49:33 -0800

    updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,12 +20,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'F.A.T.E.F.Y.'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: '.src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Finally A Text Editor For You!',
+        description: 'Take notes customized for you!',
+        background_color: '#a3d08f',
+        theme_color: '#a3d08f',
+        start_url: '/',
+        publicPath: '/',
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exlude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presents: ['@babel/present-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
